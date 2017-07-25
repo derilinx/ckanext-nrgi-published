29d0f6d3482644c9b2d84b4568f38150895aba4a
Matt Fullerton <matt.fullerton@gmail.com>
2017-07-25 20:14:51 +0000

    Correct mistake where always same field is treated

--- ckanext/nrgi/plugin.py
+++ ckanext/nrgi/plugin.py
@@ -103,7 +103,7 @@ class NrgiPlugin(plugins.SingletonPlugin):
         questions = []
         for element in ('scoring_question', 'law_practice_question', 'question', 'country', 'country_iso3', 'year'):
             newlist = []
-            for value in pkg_dict.get('question', '').replace('[', '').replace(']', '').replace('"', '').replace(' ', '').split(','):
+            for value in pkg_dict.get(element, '').replace('[', '').replace(']', '').replace('"', '').replace(' ', '').split(','):
                 newlist.append(value)
             pkg_dict[element] = newlist
         return pkg_dict
